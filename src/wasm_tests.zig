ccbf29a371b736a5923092a1c5508d6ab818360b
Andrew Gilley <andrewjamesgilley@gmail.com>
2026-08-06 13:42:39 -0400

    project update

--- src/wasm_tests.zig
+++
@@ -1,9 +0,0 @@
-const compatibility = @import("wasm/compatibility.zig");
-const manifest = @import("wasm/manifest.zig");
-const runtime = @import("wasm/runtime.zig");
-
-test {
-    _ = compatibility;
-    _ = manifest;
-    _ = runtime;
-}
